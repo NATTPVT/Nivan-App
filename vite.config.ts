5fc124ad92d91e408742386acefb430fe73ad40c
NATTPVT <mehdi.daneshi.cm@gmail.com>
2025-12-25 18:14:57 +0330

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,10 +10,12 @@ export default defineConfig(({ mode }) => {
         host: '0.0.0.0',
       },
       plugins: [react()],
-      define: {
+        //commented out because it leakes the API to any visitors
+      /* define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
+      }, 
+      */
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
