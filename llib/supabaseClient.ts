38d3c91df44c7025235e959dbc5ed22f1dafe67e
NATTPVT <mehdi.daneshi.cm@gmail.com>
2025-12-26 14:05:26 +0330

    Delete llib directory

--- llib/supabaseClient.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://colqvicukkqtiwxqdfri.supabase.co';
-const supabaseKey = 'sb_publishable_mNlmpHa-avmbeMi5g5l4yQ_7crPZbAp';
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
